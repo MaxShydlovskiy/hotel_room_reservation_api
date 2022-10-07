7b33cc76736f419ef1b70f8ffb751589e5b9bb65
Max Shydlovskiy <shidlymax@gmail.com>
2022-10-07 14:26:10 +0300

    1

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  devise :database_authenticatable,
+         :jwt_authenticatable,
+         :registerable,
+         jwt_revocation_strategy: JwtDenylist
+
+  validates_presence_of :password, :email
+  validates_length_of :password, minimum: 4, on: :create
+
+  def admin?
+    role == 'admin'
+  end
+end
---
+++ test/controllers/api/v1/accommodations_controller_test.rb
@@ -0,0 +1,38 @@
+require "test_helper"
+
+class Api::V1::AccommodationsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @api_v1_accommodation = api_v1_accommodations(:one)
+  end
+
+  test "should get index" do
+    get api_v1_accommodations_url, as: :json
+    assert_response :success
+  end
+
+  test "should create api_v1_accommodation" do
+    assert_difference("Api::V1::Accommodation.count") do
+      post api_v1_accommodations_url, params: { api_v1_accommodation: { description: @api_v1_accommodation.description } }, as: :json
+    end
+
+    assert_response :created
+  end
+
+  test "should show api_v1_accommodation" do
+    get api_v1_accommodation_url(@api_v1_accommodation), as: :json
+    assert_response :success
+  end
+
+  test "should update api_v1_accommodation" do
+    patch api_v1_accommodation_url(@api_v1_accommodation), params: { api_v1_accommodation: { description: @api_v1_accommodation.description } }, as: :json
+    assert_response :success
+  end
+
+  test "should destroy api_v1_accommodation" do
+    assert_difference("Api::V1::Accommodation.count", -1) do
+      delete api_v1_accommodation_url(@api_v1_accommodation), as: :json
+    end
+
+    assert_response :no_content
+  end
+end
