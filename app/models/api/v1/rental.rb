0648d2a35cc95177942debb97da9bb9d2f73a781
Max Shydlovskiy <shidlymax@gmail.com>
2022-10-24 15:23:01 +0300

    Date_uniqueness

--- app/models/api/v1/rental.rb
+++ app/models/api/v1/rental.rb
@@ -2,4 +2,13 @@ class Api::V1::Rental < ApplicationRecord
   enum status: [:free, :reserved, :archived]
   belongs_to :user
   belongs_to :accommodation
+  before_create :check_date_uniqueness
+
+  def check_date_uniqueness
+    if Api::V1::Rental.where(:datetime => self.datetime.to_date.beginning_of_day..self.datetime.to_date.end_of_day).exists?
+      return false
+    else
+      return true
+    end
+  end
 end
